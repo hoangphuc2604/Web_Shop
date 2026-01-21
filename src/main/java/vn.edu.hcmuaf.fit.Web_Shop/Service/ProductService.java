383251968fe4fe2c20674de9fb9ff40f9a607250
hoangphuc2604 <23130245@st.hcmuaf.edu.vn>
2026-01-21 23:19:26 +0700

    Tạo chức năng hiện danh sách sp tren trang chủ

--- src/main/java/vn.edu.hcmuaf.fit.Web_Shop/Service/ProductService.java
+++ src/main/java/vn.edu.hcmuaf.fit.Web_Shop/Service/ProductService.java
@@ -1,4 +1,22 @@
 package vn.edu.hcmuaf.fit.Web_Shop.Service;
 
+import vn.edu.hcmuaf.fit.Web_Shop.Dao.ProductDao;
+import vn.edu.hcmuaf.fit.Web_Shop.Model.Product;
+import java.util.List;
+
 public class ProductService {
+    ProductDao prDao = new ProductDao();
+
+    public List<Product> getHomeDiscountProducts(){
+        return prDao.getDiscountProducts(8);
+    }
+    public List<Product> getHomeRecommendedProducts(){
+        return prDao.getRecommendedProducts(8);
+    }
+    public List<Product> getAllDiscountProducts(){
+        return prDao.getDiscountProducts(100);
+    }
+    public List<Product> getAllRecommendedProducts(){
+        return prDao.getRecommendedProducts(100);
+    }
 }
