b18775d5ddf320deb67cc20599c713cebef707cf
phanhongphuc-sys <23130249@st.hcmuaf.edu.vn>
2026-05-30 23:05:21 +0700

    làm xong giao diện tạo đơn hàng cho hash và xác nhận chữ lý

--- src/main/webapp/js/Payment.js
+++ src/main/webapp/js/Payment.js
@@ -1,3 +1,34 @@
 function paymentSuccess(){
     alert("Bạn đã hoàn tất thủ tục thanh toán");
 }
+document.addEventListener("DOMContentLoaded", function() {
+    var modal = document.getElementById("sigModal");
+    var openBtn = document.getElementById("inputPrivateKeyBtn");
+    var closeBtn = document.getElementById("closeModalBtn");
+    var confirmBtn = document.getElementById("confirmSigBtn");
+    var sigInput = document.getElementById("sigInput");
+    openBtn.onclick = function() {
+        modal.style.display = "block";
+    }
+    closeBtn.onclick = function() {
+        modal.style.display = "none";
+    }
+    confirmBtn.onclick = function() {
+        var signature = sigInput.value.trim();
+
+        if (signature === "") {
+            alert("Vui lòng nhập chữ kí điện tử!");
+            return;
+        }
+        alert("Xác thực chữ kí thành công!");
+        modal.style.display = "none";
+
+        openBtn.innerHTML = "Đã xác thực chữ ký";
+        openBtn.style.backgroundColor = "#28a745";
+    }
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
